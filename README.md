54bb29f4dc1359ce77e2eed6e05a685ca0e4584f
J0J0 T <jojo@peek-a-boo.at>
2018-07-16 09:33:54 +0200

    add norm_wav.sh, add headline and short descr to readme

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-left_ch_m4a.sh mix_lr_m4a.sh  tag_60_funk.sh tag_fpl.sh
+= J0J0's quick and dirty audio and video utils
 
+a collection of small shell scripts I used/use to mass edit audio and video files
 
 * left_ch_m4a.sh - extracts left channel of m4a files and outputs mono wav files
 * mix_lr_m4a.sh - merges left and right channel of m4a files and outputs mono vbr mp3
+* norm_wav.sh - normalizes/amps wav files to 0db without clipping
 * tag_60_funk.sh - tags m4a files I got from a specific piano riffs book
 * tag_fpl.sh - tags m4a files I got from a specific piano audio/video course
 * tag_michel_*.sh - tags mp3 files of michel thomas spanish and french audio courses
--- norm_wav.sh
+++ norm_wav.sh
@@ -4,13 +4,6 @@ if [ "$1" == "dry" ]; then
 fi
 
 for WAV in ./*wav; do
-  # this works but better let sox detect the wavformat
-  #$DRY alac "$M4A" | sox -t raw -e signed-integer -c 1 -b 16 -r 44100 - -c 1 -b 16 "$(echo $M4A | sed -e s/.m4a/_mono.wav/g)";
-  #
-  # just spit out fileinfo -> yes alac outputs a 16b signed-int PCM wav
-  #$DRY alac "$M4A" | soxi -
-  #
-  # sox automagically detecting file type
   echo "$WAV:"
   #$DRY sox "$WAV" "$(echo $WAV | sed -e s/.wav/_amp.wav/g)" -V gain -n -l 0.1 ; # my version, what is h? add -V to see what auto fx are added by sox
   $DRY sox "$WAV" "$(echo $WAV | sed -e s/.wav/_amp.wav/g)" gain -nh dither -s ; # from manpage (same as --norm), what is dither -S, shaping or something
