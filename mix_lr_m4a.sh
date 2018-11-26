29d03534c7cebec3d29c300b749f7734e74eab18
J0J0 T <jojo@peek-a-boo.at>
2018-11-26 20:47:08 +0100

    fix DRY vars in 2 scripts

--- mix_lr_m4a.sh
+++ mix_lr_m4a.sh
@@ -6,18 +6,18 @@ fi
 
 for M4A in ./*m4a; do
   # this works but better let sox detect the wavformat
-  #$DRY alac "$M4A" | sox -t raw -e signed-integer -c 1 -b 16 -r 44100 - -c 1 -b 16 "$(echo $M4A | sed -e s/.m4a/_mono.wav/g)";
+  #$DRY alac "$M4A" | $DRY sox -t raw -e signed-integer -c 1 -b 16 -r 44100 - -c 1 -b 16 "$(echo $M4A | sed -e s/.m4a/_mono.wav/g)";
   #
   # just spit out fileinfo -> yes alac outputs a 16b signed-int PCM wav
-  #$DRY alac "$M4A" | soxi -
+  #$DRY alac "$M4A" | $DRY soxi -
   #
   # sox automagically detecting file type
   echo "### $M4A ###"
   # mix down to STEREO file
-  #$DRY alac "$M4A" | sox - -c 2 -b 16 "$(echo $M4A | sed -e s/.m4a/_both.wav/g)" remix 1-2;
+  #$DRY alac "$M4A" | $DRY sox - -c 2 -b 16 "$(echo $M4A | sed -e s/.m4a/_both.wav/g)" remix 1-2;
   # mix down to MONO file and convert to vbr mp3
   # sed version
-  #$DRY alac "$M4A" | sox - -b 16 -t wav - remix 1-2 | lame -q 0 -vbr-new -V 0 - "$(echo $M4A | sed -e s/.m4a/_both.mp3/g)"
+  #$DRY alac "$M4A" | $DRY sox - -b 16 -t wav - remix 1-2 | lame -q 0 -vbr-new -V 0 - "$(echo $M4A | sed -e s/.m4a/_both.mp3/g)"
   # parameter expansion - substring shizzle - for filename
   #echo "${M4A:0:4} ${M4A:5} ${M4A/.m4a/.mp3}"
   # parameter expansion substring - for id3 tags
--- mix_lr_mp3.sh
+++ mix_lr_mp3.sh
@@ -6,18 +6,18 @@ fi
 
 for MP3 in ./*mp3; do
   # this works but better let sox detect the wavformat
-  #$DRY alac "$MP3" | sox -t raw -e signed-integer -c 1 -b 16 -r 44100 - -c 1 -b 16 "$(echo $MP3 | sed -e s/.mp3/_mono.wav/g)";
+  #$DRY alac "$MP3" | $DRY sox -t raw -e signed-integer -c 1 -b 16 -r 44100 - -c 1 -b 16 "$(echo $MP3 | sed -e s/.mp3/_mono.wav/g)";
   #
   # just spit out fileinfo -> yes alac outputs a 16b signed-int PCM wav
-  #$DRY alac "$MP3" | soxi -
+  #$DRY alac "$MP3" | $DRY soxi -
   #
   # sox automagically detecting file type
   echo "### $MP3 ###"
   # mix down to STEREO file
-  #$DRY alac "$MP3" | sox - -c 2 -b 16 "$(echo $MP3 | sed -e s/.mp3/_both.wav/g)" remix 1-2;
+  #$DRY alac "$MP3" | $DRY sox - -c 2 -b 16 "$(echo $MP3 | sed -e s/.mp3/_both.wav/g)" remix 1-2;
   # mix down to MONO file and convert to vbr mp3
   # sed version
-  #$DRY alac "$MP3" | sox - -b 16 -t wav - remix 1-2 | lame -q 0 -vbr-new -V 0 - "$(echo $MP3 | sed -e s/.mp3/_both.mp3/g)"
+  #$DRY alac "$MP3" | $DRY sox - -b 16 -t wav - remix 1-2 | lame -q 0 -vbr-new -V 0 - "$(echo $MP3 | sed -e s/.mp3/_both.mp3/g)"
   # parameter expansion - substring shizzle - for filename
   #echo "${MP3:0:4} ${MP3:5} ${MP3/.mp3/.mp3}"
   # parameter expansion substring - for id3 tags
